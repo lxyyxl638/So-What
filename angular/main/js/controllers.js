c2707643da69ee9df5b8988effb65f594153cb35
AndyHsu <andy.xu1994@gmail.com>
2014-07-24 20:55:59 +0800

    7/24 20:55

--- angular/main/js/controllers.js
+++ angular/main/js/controllers.js
@@ -13,3 +13,19 @@ mainControllers.controller('peopleCtrl',['$scope','$http',
 	function($scope,$http){
 
 	}]);
+
+mainControllers.controller('askCtrl',['$scope','$http',
+	function($scope,$http){
+		$scope.askquestion = function(q){
+			var url = '../../CI/index.php/question_center/question_ask/format/json/';
+			$http({
+				method: 'POST',
+				url: url,
+				data: q,
+			}).success(function(response){
+                alert("提问成功");
+            }).error(function(response){
+                alert("Error!");
+            })
+		};
+	}]);
