e2fd887b14c5c9007211047f2bc086609d7d83a2
lxyyxl638 <lxyyxl638@gmail.com>
2014-07-25 13:05:15 +0800

    Revert "submit"
    This reverts commit 362570f0f3bc997f9ddd2c4ea195a3bcb5c268fb.

--- angular/app/js/controllers.js
+++ angular/app/js/controllers.js
@@ -6,34 +6,32 @@ var signControllers = angular.module('signControllers',[]);
 
 signControllers.controller('signupCtrl',['$scope','$http',
 	function($scope,$http){
+		$scope.user = {};
 
 		$scope.register = function(user){
-			var url = '../../CI/index.php/signup/usersignup/format/json/';
-			$http({
-				method: 'POST',
-				url: url,
-				data: user,
-			}).success(function(response){
-                console.log(response);
-            }).error(function(response){
-                alert("Error!");
-            })
+			$scope.user = angular.copy(user);
 		};
+
 	}]);
 
 signControllers.controller('signinCtrl',['$scope','$http',
 	function($scope,$http){
 
 		$scope.login = function(user){
-			var url = '../../CI/index.php/login/userlogin/format/json/';
+			// $scope.user = angular.copy(user);
+			var url = 'http://localhost/sowhatdemo/index.php/api/form/formu/format/json/';
+			//var data = $.param(user);
 			$http({
 				method: 'POST',
 				url: url,
 				data: user,
+				//headers: {'Content-Type': 'application/x-www-form-urlencoded'}
 			}).success(function(response){
-                console.log(response);
+                alert(response);
             }).error(function(response){
-                alert("Error!");
+                alert(response);
             })
+        
 		};
+
 	}]);
